84efafac4307361a75e20f1a5b50c86fc84f91d7
eugene <planetaryg0812@icloud.com>
2020-09-13 15:43:23 +0900

    Initial Commit

---
+++ ChatAppWithFirebase/ChatListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ChatListViewController.swift
+//  ChatAppWithFirebase
+//
+//  Created by y kimur on 2020/09/13.
+//  Copyright © 2020 y kimur. All rights reserved.
+//
+
+import UIKit
+
+class ChatListViewController:UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- ChatAppWithFirebase/SceneDelegate.swift
+++ ChatAppWithFirebase/SceneDelegate.swift
@@ -14,9 +14,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    
+        let window = UIWindow(windowScene: scene as! UIWindowScene)
+        self.window = window
+        window.makeKeyAndVisible()
+        
+        let storyboard = UIStoryboard(name: "ChatList", bundle: nil)
+        let chatListViewController = storyboard.instantiateViewController(identifier: "ChatListViewController")
+        let nav = UINavigationController(rootViewController: chatListViewController)
+        
+        window.rootViewController = nav
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
